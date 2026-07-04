c01f800184c5889a9d3363a68e62d2a47dd5f553
Prithvi-boi <prithvi.bel13@gmail.com>
2026-07-04 07:57:39 +0530

    Created Live Market Headers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import LiveMarkets from './assets/components/LiveMarkets'
+import CheckRate from './assets/components/CheckRate'
 
 function App() {
+  let [size, setSize] = useState(null)
+  let [countryAbre, setCountryAbre] = useState(null)
+  useEffect(() => {
+    async function getMarkets() {
+      const res1 = await fetch(
+        "https://api.frankfurter.dev/v1/latest?base=USD"
+      );
+
+      const today = await res1.json();
+      
+      const size = Object.keys(today.rates).length;
+      setCountryAbre(Object.keys(today.rates))
+      
+      setSize(size)
+    }
+    getMarkets()
+  }, [])
+ 
   return (
     <>
-      <div className='h-10 w-10 bg-amber-300'></div>
+      <div className='h-dvh w-dvw bg-[#0A0A0A]'>
+
+        <header>
+          <div className='flex items-center justify-between shrink-0 p-3'>
+            <img className='h-6' src="/src/assets/images/logo.svg" alt="Fx checker logo" />
+            <div className='text-[#9D9D9D] text-[0.7rem] min-w-0 truncate'>
+              {size} CURRENCIES · EOD · ECB DATA
+            </div>
+          </div>
+        </header>
+
+        <section>
+          <LiveMarkets />
+        </section>
+
+        <main>
+          <CheckRate countries={countryAbre} />
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/assets/components/LiveMarkets.jsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react'
+
+function LiveMarkets() {
+  const date = new Date()
+  const [rate, setRate] = useState(null);
+  const pairs = [
+    "JPY",
+    "INR",
+    "CHF",
+    "EUR",
+    "GBP",
+  ];
+
+
+  useEffect(() => {
+    async function getMarkets() {
+      const res1 = await fetch(
+        "https://api.frankfurter.dev/v1/latest?base=USD"
+      );
+
+      const res2 = await fetch(
+        `https://api.frankfurter.dev/v1/2026-07-${String(date.getDate() - 2).padStart(2, "0")}?base=USD`
+      );
+
+      const today = await res1.json();
+      const yesterday = await res2.json();
+
+      const markets = pairs.map((pair) => {
+        const current = today.rates[pair];
+        const previous = yesterday.rates[pair];
+        const change = current - previous;
+        const percentChange = (change / previous) * 100;
+
+        return {
+          pair: `USD/${pair}`,
+          current,
+          previous,
+          percentChange,
+        };
+      });
+
+      setRate(markets);
+    }
+
+    getMarkets();
+  }, []);
+
+  return (
+    <>
+      <div className='h-10 w-full bg-[#171719] flex '>
+        <div className='h-full grid place-items-center p-2 bg-[#CEF739] text-[#171719] text-[0.8rem] shrink-0'>● LIVE MARKETS</div>
+
+        <div className='flex h-full w-full overflow-x-scroll scrollbar-none text-[#171719] text-[0.8rem]'>
+
+          {rate?.map((r) => {
+            return (
+              <div key={r.pair} className='shrink-0 h-full p-2 border-r-2 border-[#2E2E2E] ml-1 text-[#9D9D9D] flex items-center gap-3'>
+                <h3>{r.pair}</h3>
+                <p className='text-white'>{r.current.toFixed(2)}</p>
+                <p className={r.percentChange >= 0 ? "text-green-500" : "text-red-500"}>
+                  {r.percentChange >= 0 ? "▲" : "▼"}{" "}
+                  {Math.abs(r.percentChange).toFixed(2)}%
+                </p>
+              </div>
+            )})}
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default LiveMarkets
